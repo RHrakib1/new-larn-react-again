0d31ef7837478d92021210fc8569e83589c56997
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-24 19:39:35 +0600

    added to the js fundamantals

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const increes = document.getElementById('increase')
 const decrees = document.getElementById("decrease")
 const quantity = document.getElementById("quantity")
 const total = document.getElementById("total")
+
+
+const price = 20;
+let realQuantity = 1;
+
+increes.addEventListener("click", function () {
+    realQuantity++
+    quantity.innerText = realQuantity
+})
+
+decrees.addEventListener('click', function () {
+    realQuantity--
+    quantity.innerText = realQuantity
+})
