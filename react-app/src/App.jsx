f6d3795ca6313db5645dee3cc857ba09ab261435
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-27 22:36:51 +0600

    added

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [title, setTitle] = useState()
+  const [body, setBody] = useState()
+  const [data, setData] = useState([])
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const forData = { title, body }
+    console.log(forData);
+  }
+  console.log(data);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  })
 
   return (
     <>
-      <div>
-        <h1 className='text-5xl'>hello</h1>
-      </div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title">Title</label>
+        <input id='title' className='border' type="text" onChange={(e) => setTitle(e.target.value)} />
+
+        <label htmlFor="body">Body</label>
+        <input id='body' className='border' type="text" onChange={(e) => setBody(e.target.value)} />
+        <button type='submit'>submit</button>
+      </form>
     </>
   )
 }
